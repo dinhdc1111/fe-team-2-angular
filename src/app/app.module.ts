93a0f00ffb2c49046355d0f506f26598e28114ae
Dong Cong Dinh <dinhdc1111@gmail.com>
2023-06-07 05:37:24 +0700

    refactor: :art: Refactor folders refs:TEAM2-27

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,53 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgMode
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { BaseLayoutComponent } from './components/base-layout/base-layout.component';
+import { AdminHeaderComponent } from './components/admin/admin-header/admin-header.component';
+import { AdminFooterComponent } from './components/admin/admin-footer/admin-footer.component';
+import { ClientHeaderComponent } from './components/client/client-header/client-header.component';
+import { ClientFooterComponent } from './components/client/client-footer/client-footer.component';
+import { ClientMenuComponent } from './components/client/client-menu/client-menu.component';
+import { ClientSlideComponent } from './components/client/client-slide/client-slide.component';
+import { AdminLayoutComponent } from './pages/layouts/admin-layout/admin-layout.component';
+import { ClientLayoutComponent } from './pages/layouts/client-layout/client-layout.component';
+import { AboutComponent } from './pages/client/about/about.component';
+import { ContactComponent } from './pages/client/contact/contact.component';
+import { HomeComponent } from './pages/client/home/home.component';
+import { ProductDetailComponent } from './pages/client/product-detail/product-detail.component';
+import { SigninComponent } from './pages/client/auth/signin/signin.component';
+import { SignupComponent } from './pages/client/auth/signup/signup.component';
+import { DashboardManagerComponent } from './pages/admin/dashboard-manager/dashboard-manager.component';
+import { AddProductComponent } from './pages/admin/product-manager/add-product/add-product.component';
+import { ListProductComponent } from './pages/admin/product-manager/list-product/list-product.component';
+import { EditProductComponent } from './pages/admin/product-manager/edit-product/edit-product.component';
+import { AddCategoryComponent } from './pages/admin/category-manager/add-category/add-category.component';
+import { EditCategoryComponent } from './pages/admin/category-manager/edit-category/edit-category.component';
+import { ListCategoryComponent } from './pages/admin/category-manager/list-category/list-category.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent, BaseLayoutComponent],
+  declarations: [
+    AppComponent,
+    AdminHeaderComponent,
+    AdminFooterComponent,
+    ClientHeaderComponent,
+    ClientFooterComponent,
+    ClientMenuComponent,
+    ClientSlideComponent,
+    AdminLayoutComponent,
+    ClientLayoutComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    ProductDetailComponent,
+    SigninComponent,
+    SignupComponent,
+    DashboardManagerComponent,
+    AddProductComponent,
+    ListProductComponent,
+    EditProductComponent,
+    AddCategoryComponent,
+    EditCategoryComponent,
+    ListCategoryComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
