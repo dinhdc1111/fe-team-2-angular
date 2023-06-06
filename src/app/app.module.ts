25f0f1b15b3d6c933dd883b531b52ce3ca77c21e
Dong Cong Dinh <dinhdc1111@gmail.com>
2023-06-06 21:00:31 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NzButtonModule, NzTableModule ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
