155650cf783accff4b939034a0985a410cc4e09f
Ducvpop <ducnhph22018@fpt.edu.vn>
2023-06-11 21:53:45 +0700

    style/TEAM2-23-Create-product-detail-page refs:TEAM2-23

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from '../interfaces/Product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  getProductById(id: string): Observable<IProduct> {
+    return this.http.get<IProduct>(`http://localhost:3000/api/product/${id}`);
+  }
 }
