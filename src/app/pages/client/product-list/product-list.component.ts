69948d81ce4264cde291d6fd662068eae7b6b1c6
Dong Cong Dinh <dinhdc1111@gmail.com>
2023-06-11 18:37:22 +0700

    feat/TEAM2-30-List-Product-API refs:TEAM2-30

--- src/app/pages/client/product-list/product-list.component.ts
+++ src/app/pages/client/product-list/product-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-
+  products: IProduct[] = [];
+  constructor(private productService: ProductService) {
+    this.productService.getAll().subscribe((data) => {
+      this.products = data.product;
+    });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from '../interfaces/Product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor() { }
+  API_URL: string = `http://localhost:3000/api`;
+  constructor(private http: HttpClient) {}
+  getAll(): Observable<{ product: IProduct[] }> {
+    return this.http.get<{ product: IProduct[] }>(`${this.API_URL}/product`);
+  }
 }
